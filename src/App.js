1a2a93edcfdcc767080598e040c9c18cd22fc31c
Kasim <kasimalam@gmail.com>
2019-12-01 19:07:45 -0500

    windows first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">LOLZss</div>;
+  return <div>This is the start of the local experience clone</div>;
 }
 
 export default App;
